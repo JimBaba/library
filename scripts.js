99d49be9d07bba43aa60ce43a5c6a9a636f9bbf3
Daniel Aalberts <aalberts.daniel@gmail.com>
2021-04-07 20:15:48 +0200

    added onclick to autobtn

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const container = document.getElementsByClassName("book-container")
+const container = document.getElementById("book-container")
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -23,12 +23,27 @@ const akira = new Book("AKIRA", "Katsuhiro Otomo", 478, true)
 
 const myLibrary = [dreiSonnen, urknall, akira];
 
+
 function addToLibrary(book) {
     myLibrary.push(book)
     return book + " added to myLibrary"
 }
 
+function newBook(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook);
+}
+
 function showBooks() { 
+    for(let i = 0; i < myLibrary.length; i++ ){
+        let cell = document.createElement("div");
+        cell.style.setProperty("grid-column", 1)
+        cell.style.setProperty("border", "solid black");
+        cell.style.setProperty("margin", "5px")
+        cell.innerText = myLibrary[i].info();
+        container.appendChild(cell).className = "book" + i;
+        makeBtn();
+    }
     // loops through myLibrary and 
     // display the books (titles?) on the website
     // how to display them? grid? divs?
@@ -38,3 +53,15 @@ function showBooks() {
 
 // add button to remove individual books from myLibrary from the display
 
+
+
+function makeBtn() {
+    let delBtn = document.createElement("input");
+    delBtn.style.setProperty("grid-column", 2)
+    delBtn.style.setProperty("width","100px")
+    delBtn.style.setProperty("margin", "5px")
+    delBtn.type = "button";
+    delBtn.value = "delete";
+    delBtn.onclick = function() {myLibrary.some}
+    container.appendChild(delBtn).id = "btnDel";
+}
