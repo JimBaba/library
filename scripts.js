d0684c2c6c27a480a4374f6ec34be428782e02bd
Daniel Aalberts <aalberts.daniel@gmail.com>
2021-04-16 14:27:54 +0200

    missing working form

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 const container = document.getElementById("book-container")
+const br = document.createElement("br")
+const formCont = document.getElementById("newBook")
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        return title + " by " + author + ", " + pages + " Seiten, " 
-        + "gelesen: " + read       
-    }
     this.announce = function(){
         return "This is a book"
+    }
+    this.switchRead = function(){
+        if(this.read == true){
+            this.read = false
+            console.log("change status to false " + this.read)
+        } else {
+            this.read = true
+            console.log("change status to true " + this.read)
+        }
+    }
+    this.info = function(){
+        return this.title + " by " + this.author + ", " + this.pages + " Seiten, " 
+        + "gelesen: " + this.read       
     }
     
 }
@@ -29,7 +40,11 @@ function addToLibrary(book) {
     return book + " added to myLibrary"
 }
 
-function newBook(title, author, pages, read){
+function newBook(){
+    let title = prompt("Enter title");
+    let author = prompt("Enter author");
+    let pages = prompt("How many pages?");
+    let read = prompt("Did you read it? (true or false)")
     let newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook);
 }
@@ -41,8 +56,9 @@ function showBooks() {
         cell.style.setProperty("border", "solid black");
         cell.style.setProperty("margin", "5px")
         cell.innerText = myLibrary[i].info();
-        container.appendChild(cell).className = "book" + i;
-        makeBtn();
+        container.appendChild(cell).id = "book" + i;
+        makeBtnDel(i);
+        makeBtnRead(i);
     }
     // loops through myLibrary and 
     // display the books (titles?) on the website
@@ -55,13 +71,91 @@ function showBooks() {
 
 
 
-function makeBtn() {
+function makeBtnDel(i) {
     let delBtn = document.createElement("input");
     delBtn.style.setProperty("grid-column", 2)
     delBtn.style.setProperty("width","100px")
+    delBtn.style.setProperty("height", "40px")
     delBtn.style.setProperty("margin", "5px")
     delBtn.type = "button";
     delBtn.value = "delete";
-    delBtn.onclick = function() {myLibrary.some}
-    container.appendChild(delBtn).id = "btnDel";
+    delBtn.onclick = function() {
+        myLibrary.splice(i,1)
+        let delBtn = document.getElementById("btnDel" + i) 
+        container.removeChild(delBtn)
+        let delBook = document.getElementById("book" + i)
+        container.removeChild(delBook)
+        let delRead = document.getElementById("readBtn" + i)
+        container.removeChild(delRead);
+    }
+    container.appendChild(delBtn).id = "btnDel" + i;
 }
+
+function makeBtnRead(i) {
+    let readBtn = document.createElement("input");
+    readBtn.style.setProperty("grid-column", 3);
+    readBtn.style.setProperty("width", "100px");
+    readBtn.style.setProperty("height", "40px")
+    readBtn.style.setProperty("margin", "5px");
+    readBtn.type = "button";
+    readBtn.value = "Read";
+    readBtn.onclick = function() {
+        delAll();
+        console.log("delete all")
+        myLibrary[i].switchRead()
+        console.log(myLibrary[i].info() + " " + myLibrary[i].read)
+        showBooks();
+        console.log("show new")
+    }
+    container.appendChild(readBtn).id = "readBtn" + i;
+    
+}
+
+function createForm() {
+    let form = document.createElement("form");
+    form.setAttribute("method", "post")
+    form.setAttribute("action", "submit.php")
+    form.id = "formBook";
+    let title = document.createElement("input")
+    title.setAttribute("type", "text")
+    title.setAttribute("name", "booktitle")
+    title.setAttribute("placeholder", "Book Title")
+    form.appendChild(title)
+    form.appendChild(br.cloneNode());
+    let author = document.createElement("input")
+    author.setAttribute("type", "text")
+    author.setAttribute("name", "author")
+    author.setAttribute("placeholder", "Author")
+    form.appendChild(author)
+    form.appendChild(br.cloneNode())
+    let pages = document.createElement("input")
+    pages.setAttribute("type", "text")
+    pages.setAttribute("name", "amountPages")
+    pages.setAttribute("placeholder", "Amount of Pages")
+    form.appendChild(pages)
+    form.appendChild(br.cloneNode())
+    let read = document.createElement("input")
+    read.setAttribute("type", "text")
+    read.setAttribute("name", "read")
+    read.setAttribute("placeholder", "Already read? true or false")
+    form.appendChild(read)
+    form.appendChild(br.cloneNode())
+    let submit = document.createElement("input")
+    submit.setAttribute("type", "submit")
+    submit.setAttribute("value", "submit")
+    submit.onclick = function() {
+        // addToLibrary(new Book(nForm.elements[0].value, nForm.elements[1].value,
+        //    nForm.elements[2].value, nForm.elements[3].value))
+        console.log("onclick works")
+    }
+    form.appendChild(submit)
+    form.appendChild(br.cloneNode());
+    formCont.appendChild(form)
+    let nForm = document.getElementById("formBook")
+}
+
+function delAll() {
+    container.innerHTML = ""
+}
+
+
